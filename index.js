a6c2e2791ea17a9bbb0262e65d6e786362a45f0e
Blake Schwartz <blakehschwartz@gmail.com>
2016-10-06 20:42:42 -0700

    Adds api services for Stormpath Notes app

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var stormpath = require('express-stormpath');
+var bodyParser = require('body-parser')
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(stormpath.init(app, {
+  expand: {
+    customData: true,
+  },
+  web: {
+    produces: ['application/json']
+  }
+}));
+
+app.get('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
+  res.json({notes: req.user.customData.notes || 'This is your notebook. Start editing and saving notes.'})
+});
+
+app.post('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
+  if (!req.body.notes || typeof req.body.notes != 'string') {
+    res.status(400).send('400 - Bad Request');
+  }
+  req.user.customData.notes = req.body.notes;
+  req.user.customData.save();
+  res.status(200).end();
+});
+
+app.listen(3000);
